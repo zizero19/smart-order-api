172ed794f839ec05ed8aef895123e09574da0e27
zizero19 <cicero.ferro.78@gmail.com>
2026-02-18 17:19:22 -0300

    Feat: Adicionado anotações e profile de desenvolvimento em ambiente DEV

--- src/main/java/com/smartorder/api/ApiApplication.java
+++ src/main/java/com/smartorder/api/ApiApplication.java
@@ -1,9 +1,17 @@
 package com.smartorder.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+@Profile("dev")
 public class ApiApplication {
 
 	public static void main(String[] args) {
